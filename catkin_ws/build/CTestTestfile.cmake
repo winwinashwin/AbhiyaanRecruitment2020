f33c79f1c5bd3ab1724f2f4057c45558d8fbd22f
Ashwin A Nayar <ashwin5059198@gmail.com>
2020-03-23 16:58:01 +0530

    created catkin workspace

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ashwin_a_nayar/github_abhi/catkin_ws/src
+# Build directory: /home/ashwin_a_nayar/github_abhi/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
---
+++ catkin_ws/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/gmock
+# Build directory: /home/ashwin_a_nayar/github_abhi/catkin_ws/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
